f8f4ec10f8835cd9a215001b3b523ddad3d375d3
ArhaanTheCoder <yusufzaiarhaan@gmail.com>
2025-08-23 21:06:05 +0100

    Create codes with hashtags/comments:
    Here’s a fun Python program that lets you have a conversation with the computer!
    What it does:
    
    (1)      Asks for your name and greets you: Hello, your name!
    
    (2)     Asks for your favorite color and responds: Wow! your                                                       favorite color is a nice color!
    
    Perfect for beginners who want to learn how to use input and print in Python.

---
+++ code_with_hastags_or_comments.py
@@ -0,0 +1,11 @@
+# This tutorial teaches beginners how to get input from the user
+
+# Ask the user for their name
+name = input("What is your name? ")  # input() waits for the user to type something
+
+# Greet the user using the name they typed
+print("Hello, " + name + "!")  # prints a personalized greeting
+
+# Optional: ask for their favorite color
+color = input("What is your favorite color? ")
+print("Wow! " + color + " is a nice color!")  # responds with the color
