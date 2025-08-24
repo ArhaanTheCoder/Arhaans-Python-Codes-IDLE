174deed35db5d6050a34d240e9b8871a6ad29861
ArhaanTheCoder <yusufzaiarhaan@gmail.com>
2025-08-24 10:50:03 +0100

    Create a simple calculator in Python!
    Heres how you can make a very easy-made calculator in Python!
    You can change the code to add, multiply, subtract and divide! Here are the symbols for each one:
    
    + = addition
    - = subtraction
    * = multiplication
    / = divide

---
+++ simple_calculator.py
@@ -0,0 +1,9 @@
+# Ask for two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Add them together
+sum_result = num1 + num2
+
+# Show the result
+print("The sum is:", sum_result)
